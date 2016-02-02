b8befebf2a1950ddb341dbc21208eb98992c68dc
TastefulRug <TastefulRug@users.noreply.github.com>
2016-02-02 13:30:18 -0500

    Python2 Exercises
    Created a repository for the exercises from a Python 2 tutorial I'm
    working through.

---
+++ ex3.py
@@ -0,0 +1,25 @@
+#display the string contained in the quotes ""
+print "I will now count my chickens:"
+#display the string Hens, then resolve the math expressions and display the result
+print "Hens", 25.0 + 30 / 6
+#display "Roosters" followed by the result of 100 minus 4% of 25 times 3
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
+
+print "Now I will count the eggs:"
+
+print 3.0 + 2.0 + 1.0 -5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+print "Is it true that 3 + 2 < 5 - 7?"
+#check whether the inequality is True or False and print the answer
+print 3 + 2 < 5-7
+#etc. for all the rest
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
