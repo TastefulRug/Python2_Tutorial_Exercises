37cd6d778a95708fa499fe186ab6ff02848e77a7
TastefulRug <TastefulRug@users.noreply.github.com>
2016-02-03 19:29:49 -0500

    Python Ex9
    Completed Exercise 9: Printing, Printing, Printing

---
+++ ex9.py
@@ -0,0 +1,18 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+
+# \n is a new line character
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJuly\nAug"
+
+print "Here are the days: ", days
+print "Here are the months: ", months
+
+# Triple quotes """ """ let you make a block of text for the print command
+print """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
+
+# Errors:
+# mistyped \n and left out the n
