ecd8d36217647fbab691958eba2eb8a76bc8c6cd
TastefulRug <TastefulRug@users.noreply.github.com>
2016-02-06 20:49:19 -0500

    Python Ex13 Study Drill
    Messed around with an IF statement and a FOR statement.

--- ex13.py
+++ ex13.py
@@ -7,7 +7,17 @@ print "Your first variable is:", first
 print "Your second variable is:", second
 print "Your third variable is:", third
 
-var1 = raw_input(script+"? ")
-# Adding a space inside the raw_input() prompt required
-# a + sign, ie. string addition. A comma didn't work
-print "You typed %r." % var1
+# Seeing if input matches a variable
+var1 = raw_input("What is the name of this script? ")
+if var1 == script:
+	print "Correct!"
+# Did this to figure out how Python does if statements
+else:
+	print "Wrong! It's %s." % script
+# Printing the argument variable
+print argv
+
+string = 'Hellow World'
+# For each character in the string, print that character
+for x in string:
+	print x
